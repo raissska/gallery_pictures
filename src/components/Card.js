229f4297123b6fda89a701ef7a9bff1f8726a139
Raisa Myronova <raisam@rubitax.com>
2023-02-03 19:33:59 +0200

    changed style and effect for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ export const Card = ({ img }) => {
   return (
 
     <div
-    data-aos="zoom-in" data-aos-duration="3000" data-aos-delay="50"
-      className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-      <div className="card p-0 overflow-hidden h-100 shadow">
-        <img src={img.download_url} width="100%" height="300" className="card-img-top" alt="..." />
+      data-aos="fade-up" data-aos-duration="2000" data-aos-delay="50"
+      className="col-10 col-md-6 col-lg-3 mx-0 mb-4">
+      <div className="card p-0 overflow-hidden  shadow">
+        <img src={img.download_url} width="100%" height="250" className="card-img-top object-fit-cover" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{img.author}</h5>
           <p className="card-text">desc</p>
